9e07a152b11a78ca6ebbf6438a36c72b304834a1
rajaratnamrevu <rajaratnamrevu@gmail.com>
2026-06-02 20:03:59 +0530

    active and completed tasks are added

--- TodoList/src/components/DoneTask.jsx
+++ TodoList/src/components/DoneTask.jsx
@@ -1,8 +1,8 @@
 export default function Donetodo(props) {
     return(
         <button 
-    className="btn btn-success"
+    className="btn btn-success mx-2"
     onClick={props.iscompleted}
-    >Done</button>
+    ><i className="bi bi-check2-square"></i></button>
     )
 }
